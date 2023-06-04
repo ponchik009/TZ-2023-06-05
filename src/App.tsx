6c2ed9bc1f7a90c2bcc77fa6f1bb6c49e6ded497
ponchik009 <lolikonru@mail.ru>
2023-06-05 00:01:24 +0800

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -74,58 +74,6 @@ function App() {
   }, []);
 
   return <RouterProvider router={router} />;
-
-  return (
-    <div className="App">
-      <span>1101826888</span>
-      <span>3154111f359e4e5b8ea48b32eb99c5a9d2b532566dad4d19ad</span>
-      <span>79642733069</span>
-      <input
-        type="text"
-        placeholder="idInstance"
-        value={idInstanceInput}
-        onChange={(e) => setIdInstanceInput(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="apiTokenInstance"
-        value={apiTokenInstanceInput}
-        onChange={(e) => setApiTokenInstanceInput(e.target.value)}
-      />
-      <button onClick={setCredentials}>SET CREDENTIALS</button>
-
-      <input
-        type="text"
-        placeholder="phone"
-        value={phoneInput}
-        onChange={(e) => setPhoneInput(e.target.value)}
-      />
-      <button onClick={createChatClick}>CREATE CHAT</button>
-      <div>current chat: {currentChat}</div>
-
-      <input
-        type="text"
-        placeholder="message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendMessageClick}>SEND MESSAGE</button>
-
-      <div>Session:</div>
-      {Object.entries(session).map(([phone, chat]) => {
-        return (
-          <div>
-            {chat.contactName}
-            <ul>
-              {chat.messages.map((m) => (
-                <li>{m.text}</li>
-              ))}
-            </ul>
-          </div>
-        );
-      })}
-    </div>
-  );
 }
 
 export default App;
